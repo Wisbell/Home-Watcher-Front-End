6548e205d87e5fadf4ed4ef1ef9ebc04cddc06c1
Wisbell <westleyisbell@gmail.com>
2017-05-16 20:33:06 -0500

    Finished login partial and about partial

--- app/app.js
+++ app/app.js
@@ -1,14 +1,23 @@
 const app = angular.module('HomeWatcher', ['ngRoute'])
 
 app.config(['$routeProvider', function($routeProvider){
+
   $routeProvider
   .when('/', {
     templateUrl: 'partials/home.html',
     controller: 'HomeCtrl'
   })
+  .when('/login', {
+    templateUrl: 'partials/login.html',
+    controller: 'AuthCtrl'
+  })
   .when('/recent', {
     templateUrl: 'partials/recent.html',
     controller: 'RecentCtrl'
+  })
+  .when('/about', {
+    templateUrl: 'partials/about.html',
+    controller: 'AboutCtrl'
   })
   .otherwise({
     redirectTo: '/'
--- app/controllers/recentCtrl.js
+++ app/controllers/recentCtrl.js
@@ -1,9 +1,17 @@
-app.controller('RecentCtrl', function(PicFactory, $scope) {
+app.controller('RecentCtrl', function(PicFactory, $scope, AuthFactory, $location) {
   $scope.stuff = "stuff"
 
-  PicFactory.getAllPictures()
-  .then( (pictures) => {
-    $scope.pictures = pictures
-    $scope.$apply()
-  })
+  let loggedIn = AuthFactory.loggedIn
+  console.log("logged", loggedIn)
+
+  if(loggedIn) {
+    PicFactory.getAllPictures()
+    .then( (pictures) => {
+      $scope.pictures = pictures
+      $scope.$apply()
+    })
+
+  } else {
+    $location.url('/login')
+  }
 })
