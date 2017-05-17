44445c4b77425cf8bad426741edda5f37886cef4
Wisbell <westleyisbell@gmail.com>
2017-05-16 20:36:12 -0500

    Created version of app that requires no authentication to see pictures

--- app/controllers/recentCtrl.js
+++ app/controllers/recentCtrl.js
@@ -1,17 +1,9 @@
 app.controller('RecentCtrl', function(PicFactory, $scope, AuthFactory, $location) {
-  $scope.stuff = "stuff"
 
-  let loggedIn = AuthFactory.loggedIn
-  console.log("logged", loggedIn)
-
-  if(loggedIn) {
-    PicFactory.getAllPictures()
+  PicFactory.getAllPictures()
     .then( (pictures) => {
       $scope.pictures = pictures
       $scope.$apply()
     })
 
-  } else {
-    $location.url('/login')
-  }
 })
